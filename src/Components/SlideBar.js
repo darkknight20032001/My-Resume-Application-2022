9826f4ddec5e9a20f6795b8b3cc46d9c02d852b6
darkknight20032001 <84170745+darkknight20032001@users.noreply.github.com>
2022-04-08 13:12:08 +0530

    Made by Rachit Verma

--- src/Components/SlideBar.js
+++ src/Components/SlideBar.js
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SlideBar.css";
 import { Link } from "react-router-dom";
 const SlideBar = () => {
+  const [Toggle, setToggle] = useState(false);
+
+  Toggle ? console.log("Done") : console.log("Not Done");
+
   return (
-
-      <ul className="navbar">
-        <li>
-          <Link to="/" >Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/myprojects">My Projects</Link>
-        </li>
-        <li>
-          <Link to="/contactme">Contact Me</Link>
-        </li>
-        <div className="burger">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </ul>
-
+    <ul className="navbar">
+      <li className="navbar">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="navbar">
+        <Link to="/about">About</Link>
+      </li>
+      <li className="navbar">
+        <Link to="/myprojects">My Projects</Link>
+      </li>
+      <li className="navbar">
+        <Link to="/contactme">Contact Me</Link>
+      </li>
+      <div
+        className="burger"
+        onClick={() => {
+          setToggle(!Toggle);
+        }}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        {Toggle && (
+          <ul className="ul-toggle">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/myprojects">My Projects</Link>
+            </li>
+            <li>
+              <Link to="/contactme">Contact Me</Link>
+            </li>
+          </ul>
+        )}
+      </div>
+    </ul>
   );
 };
 
